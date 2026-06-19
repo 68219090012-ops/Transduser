83eb7676b5323f8a12b8e27062b9cef3bcdf8eb5
69319090005-create <69319090005@technictrang.ac.th>
2026-06-19 14:54:48 +0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,15 @@
-#include <Arduino.h>
+﻿#include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+const int ledPin = 13; // ขา LED บนบอร์ด Arduino UNO
+const unsigned long blinkInterval = 500; // ระยะเวลาเปิด/ปิด 500 ms
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  pinMode(ledPin, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  digitalWrite(ledPin, HIGH); // เปิดไฟ LED
+  delay(blinkInterval);
+  digitalWrite(ledPin, LOW); // ปิดไฟ LED
+  delay(blinkInterval);
 }
