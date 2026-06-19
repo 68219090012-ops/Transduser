6eeae5bed31e5dca82a0a8a243ee8ab8b87222c1
69319090005-create <69319090005@technictrang.ac.th>
2026-06-19 15:38:14 +0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,20 @@
 ﻿#include <Arduino.h>
 
-const int ledPin = 13; // ขา LED บนบอร์ด Arduino UNO
+const int led1Pin = 12;
+const int led2Pin = 13;
 const unsigned long blinkInterval = 500; // ระยะเวลาเปิด/ปิด 500 ms
 
 void setup() {
-  pinMode(ledPin, OUTPUT);
+  pinMode(led1Pin, OUTPUT);
+  pinMode(led2Pin, OUTPUT);
 }
 
 void loop() {
-  digitalWrite(ledPin, HIGH); // เปิดไฟ LED
+  digitalWrite(led1Pin, HIGH); // เปิดไฟ LED1
+  digitalWrite(led2Pin, HIGH); // เปิดไฟ LED2
   delay(blinkInterval);
-  digitalWrite(ledPin, LOW); // ปิดไฟ LED
+
+  digitalWrite(led1Pin, LOW); // ปิดไฟ LED1
+  digitalWrite(led2Pin, LOW); // ปิดไฟ LED2
   delay(blinkInterval);
 }
