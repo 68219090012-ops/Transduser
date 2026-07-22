78fdadbebb0702b5a4ace72964fcea3810a7e501
69319090005-create <68219090012@technictrang.ac.th>
2026-07-22 14:10:45 +0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,62 @@
-﻿#include <Arduino.h>
+﻿/*
+  ระบบตรวจจับเปลวไฟและเปิดพัดลมดูดควัน
+  แสดงผลผ่าน Serial Monitor
 
-const int ledPin = 13; // ขา LED บนบอร์ด Arduino UNO
-const unsigned long blinkInterval = 500; // ระยะเวลาเปิด/ปิด 500 ms
+  KY-026
+  VCC -> 5V
+  GND -> GND
+  DO  -> D7
+
+  Relay
+  IN  -> D8
+*/
+#include <Arduino.h>
+#define FLAME_PIN 7
+#define RELAY_PIN 8
 
 void setup() {
-  pinMode(ledPin, OUTPUT);
+
+  pinMode(FLAME_PIN, INPUT);
+  pinMode(RELAY_PIN, OUTPUT);
+
+  digitalWrite(RELAY_PIN, LOW);
+
+  Serial.begin(9600);
+
+  Serial.println("=======================================");
+  Serial.println("   Flame Detection System (KY-026)");
+  Serial.println("=======================================");
+  Serial.println("System Ready...");
+  Serial.println();
 }
 
 void loop() {
-  digitalWrite(ledPin, HIGH); // เปิดไฟ LED
-  delay(blinkInterval);
-  digitalWrite(ledPin, LOW); // ปิดไฟ LED
-  delay(blinkInterval);
+
+  int flame = digitalRead(FLAME_PIN);
+
+  Serial.println("-------------------------------");
+
+  if (flame == LOW) {
+
+    digitalWrite(RELAY_PIN, HIGH);
+
+    Serial.println("Status      : WARNING!");
+    Serial.println("Fire        : DETECTED");
+    Serial.println("Relay       : ON");
+    Serial.println("Exhaust Fan : ON");
+  }
+  else {
+
+    digitalWrite(RELAY_PIN, LOW);
+
+    Serial.println("Status      : NORMAL");
+    Serial.println("Fire        : NOT DETECTED");
+    Serial.println("Relay       : OFF");
+    Serial.println("Exhaust Fan : OFF");
+  }
+
+  Serial.println("-------------------------------");
+  Serial.println();
+
+  delay(1000);
 }
